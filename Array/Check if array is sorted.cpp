a2800e0b4da056b72be41c8774d413f242ad76c8
Pranjal Nikhade <94614180+pn3425@users.noreply.github.com>
2025-05-22 23:26:54 +0530

    Create Check if array is sorted.cpp
    Given an array arr[], check whether it is sorted in non-decreasing order. Return true if it is sorted otherwise false.
    
    Non descending - Non-descending order means a sequence where each element is greater than or equal to the one before it.

---
+++ Array/Check if array is sorted.cpp
@@ -0,0 +1,20 @@
+// User function template for C++
+
+class Solution {
+  public:
+    bool arraySortedOrNot(vector<int>& arr) {
+        // code here
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i]>=arr[i-1])
+            {
+                
+            }
+            else
+            {
+            return false;
+            }
+        }
+        return true;
+    }
+};
